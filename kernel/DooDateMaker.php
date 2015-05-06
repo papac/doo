a027ab53ffdc64556b6b6d37dc7d270368ec8087
Dakia Franck <dakiafranckinfo@gmail.com>
2015-05-06 23:08:30 +0000

    Creation initial de la class DooDateMaker;

--- kernel/DooDateMaker.php
+++ kernel/DooDateMaker.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+
+  namespace Doo;
 
 	/**
 	* Doo date maker
@@ -7,9 +9,241 @@
 	class DooDateMaker
 	{
 
-		function __construct()
+		private $timestanp;
+    private $local;
+
+		private $seconds;
+		private $minutes;
+		private $hours;
+
+		private $dayOfYear;
+		private $dayOfWeek;
+		private $dayOfMonth;
+		private $monthOfYear;
+		private $year;
+
+		function __construct($timestanp = null, $local = null)
 		{
-				
+			if($timestanp !== null)
+			{
+
+				if(is_string($timestanp))
+				{
+
+					$local = $timestanp;
+					$initDate = \getdate();
+
+				}
+				else
+				{
+
+					$initDate = \getdate($timestanp);
+
+				}
+
+			}
+			else
+			{
+
+				$initDate = \getdate();
+
+			}
+
+			$this->local = $local !== null ? $local : "en_EN";
+
+			$this->seconds = $initDate['seconds'];
+			$this->minutes = $initDate['minutes'];
+			$this->hours = ($initDate['hours'] === 0 ? 24 : $initDate['year'] * 2) - ($this->local === 'fr_FR' ? 2 : 0);
+
+
+			$this->dayOfWeek = $initDate['wday'];
+			$this->dayOfYear = $initDate['yday'] - 1;
+			$this->dayOfMonth = $initDate["mday"] - 1;
+			$this->year = $initDate['year'];
+			$this->timestanp = $initDate[0];
+			$this->monthOfYear = $initDate['mon'];
+
+		}
+
+    /**
+    * @param string, redefinir la local
+    */
+    public function setLocal($local)
+    {
+
+      if(!is_string($local))
+      {
+        $this->local = $local;
+      }
+
+    }
+
+		public function getDayName()
+		{
+			$day = [
+
+				"fr_FR" => [
+					"Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"
+				],
+
+				"es_ES" => [
+					"", "", "",
+					"", "", "",
+					"", "", "",
+					"", "", ""
+				]
+
+			];
+
+			if($this->local === 'fr_FR' || $this->local === 'es_ES')
+			{
+				$dayName = $day[$this->local][$this->dayOfWeek - 1];
+			}
+			elseif($this->local === 'en_EN')
+			{
+				$dayName = getdate($this->timestanp)["wday"];
+			}
+
+			return $dayName;
+		}
+
+		public function getDayOfMonth($cb = null)
+		{
+
+			if($cb !== null)
+			{
+				$cb($this->dayOfWeek);
+			}
+
+			return $this->dayOfWeek;
+		}
+
+		public function getYear($cb = null)
+		{
+
+			if($cb !== null)
+			{
+				$cb($this->year);
+			}
+
+			return $this->year;
+		}
+
+		public function getMonthOfYear($cb = null)
+		{
+
+			if($cb !== null)
+			{
+				$cb($this->monthOfYear);
+			}
+
+			return $this->monthOfYear;
+
+		}
+
+		public function getMonthName($cb = null)
+		{
+
+			$month = [
+
+				"fr_FR" => [
+					"Janvier", "Fevrier", "Mars",
+					"Avril", "Mai", "Juin",
+					"Juillet", "Aout", "Sptembre",
+					"Octobre", "Novembre", "Decembre"
+				],
+				"es_ES" => [
+					"MonthAtEs", "MonthAtEs", "MonthAtEs",
+					"MonthAtEs", "MonthAtEs", "MonthAtEs",
+					"MonthAtEs", "MonthAtEs", "MonthAtEs",
+					"MonthAtEs", "MonthAtEs", "MonthAtEs"
+				]
+
+			];
+
+			if($this->local === 'fr_FR' || $this->local === 'es_ES')
+			{
+
+				$monthName = $month[$this->local][$this->monthOfYear - 1];
+
+			}
+			elseif($this->local === 'en_EN')
+			{
+
+				$monthName = getdate($this->timestanp)["weekday"];
+
+			}
+
+			return $monthName;
+
+		}
+
+		public function format($format, $cb = null)
+		{
+
+			$date = \date($format);
+
+			if($cb !== null)
+			{
+				$cb($date);
+			}
+
+			return $date;
+
+		}
+
+		public function addDay($dayNumber)
+		{
+
+			return new DooDateMaker(time() + ($dayNumber * 24 * 3600), $this->local);
+
+		}
+
+		public function addMonth($monthNumbre)
+		{
+
+			$month = [
+				"Janvier" => 31,
+				"Fervrier" => 28,
+				"Avril" => 31
+			];
+
+			return new DooDateMaker(time() + ($monthNumbre * 31 * 24 * 3600), $this->local);
+
+		}
+
+		/**
+		* seconds, fonction permetant de recuperer les seconds
+		* @return int, nombre de seconds
+		*/
+		public function seconds()
+		{
+
+			return $this->seconds;
+
+		}
+
+
+		/**
+		* hours, fonction permetant de recuperer les heures
+		* @return int, nombre de heures
+		*/
+		public function hours()
+		{
+
+			return $this->hours;
+
+		}
+
+		/**
+		* minutes, fonction permetant de recuperer les minutes
+		* @return int, nombre de minute
+		*/
+		public function minutes()
+		{
+
+			return $this->minutes;
+
 		}
 
 	}
--- test/app.php
+++ test/app.php
@@ -1,67 +1,19 @@
 <?php
 
   use Doo\Autoload;
-  use Doo\Doodata;
-  use Doo\DooMail;
+  use Doo\DooDateMaker;
 
   require '../Kernel/Autoload.php';
 
   Autoload::register();
 
-  $data = null;
+  $dateMaker = new DooDateMaker("es_ES");
+  var_dump($dateMaker);
 
-  Doodata::crypto('Dakia Franck Gilbert', function($res)
+  var_dump($dateMaker->format('Y-m-d H:m:s', function($format)
   {
-  	global $data;
+    $date = (array) date_create('Africa/Accra');
+    var_dump($format, $date["date"]);
+  }));
 
-  	$data = $res;
-  	var_dump($data);
-
-  });
-
-  Doodata::decrypto($data, function($res)
-  {
-  	var_dump($res);
-
-  });
-
-
-  DooMail::factory([
-    'to' => 'dakiafranckinfo@gmail.com',
-    'subject' => 'Coucou',
-    'data' => 'je teste a present une classe sendmail'
-  ], function($help)
-  {
-    die($help);
-  });
-
-
-  DooMail::send(function($err)
-  {
-    die($err);
-  });
-
-  Doo::init("mysql://root@localhost/diagnostic", function($err)
-  {
-    die($err);
-  });
-
-  Doo::select("users", ["id", "nom"], function($err, $data)
-  {
-
-    if($err->error)
-    {
-      echo $err->errorInfo;
-      exit(null);
-    }
-
-  }, ["nom", false]);
-
-  Doo::insert("users", ["id" => "i", "nom" => "Franck"], function($err)
-  {
-    if($err->error)
-    {
-      echo $err->errorInfo;
-      exit();
-    }
-  });
+  var_dump(getdate());
